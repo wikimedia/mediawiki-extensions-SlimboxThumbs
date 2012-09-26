7bf3c6a1207ea7a7991be73ee7e23421029351f1
Vitaliy Filippov <vitalif@yourcmc.ru>
2012-09-26 17:31:06 +0400

    SlimboxThumbs extension (lightbox-like picture viewer for MediaWiki), rewritten and revived.
    Features:
    * Click on every image thumbnail on the page produces a lightbox with full-size image;
    * Big full-size images are scaled to 90% of browser window automatically;
    * Removed annoying animation of image caption - originally it moved up and down on every image click;
    * Bugs fixed (non-ascii picture names now work, etc);
    * License changed to GPLv3 as original CC-BY-SA was incompatible with MediaWiki's license (!!!);
    * Cleaner extension code (see http://svn.wikimedia.org/viewvc/mediawiki/trunk/extensions/SlimboxThumbs/?pathrev=107261 for reference).
    
    Change-Id: I7ee6d962110f7b7e69d0385977c3f6a7ab21a548

---
+++ SlimboxThumbs.php
@@ -0,0 +1,97 @@
+<?php
+
+/**
+ * SlimboxThumbs extension /REWRITTEN/
+ * Originally http://www.mediawiki.org/wiki/Extension:SlimboxThumbs
+ * Now it does the same, but the code is totally different
+ * Required MediaWiki: 1.13+
+ *
+ * This extension includes a copy of Slimbox.
+ * It has one small modification: caption is animated together
+ * with image container, instead of original annoying consecutive animation.
+ * Also "autoloader" is removed from slimbox2.js, and there is an additional
+ * slimboxthumbs.js file.
+ *
+ * You can however get your own copy of Slimbox and use it by replacing the
+ * included one: http://www.digitalia.be/software/slimbox2
+ *
+ * @license GNU GPL 3.0 or later: http://www.gnu.org/licenses/gpl.html
+ * CC-BY-SA should not be used for software, moreover it's incompatible with GPL, and MW is GPL.
+ *
+ * @file SlimboxThumbs.php
+ *
+ * @author Vitaliy Filippov <vitalif@mail.ru>
+ */
+
+if ( !defined( 'MEDIAWIKI' ) ) {
+	die( 'Not an entry point.' );
+}
+
+define( 'SlimboxThumbs_VERSION', '2012-09-17' );
+
+// Register the extension credits.
+$wgExtensionCredits['other'][] = array(
+	'path' => __FILE__,
+	'name' => 'SlimboxThumbs',
+	'url' => 'https://www.mediawiki.org/wiki/Extension:SlimboxThumbs',
+	'author' => array(
+		'[http://yourcmc.ru/wiki/User:VitaliyFilippov Vitaliy Filippov], ' .
+		'[http://www.mediawiki.org/wiki/User:Jeroen_De_Dauw Jeroen De Dauw].'
+	),
+	'descriptionmsg' => 'slimboxthumbs-desc',
+	'version' => SlimboxThumbs_VERSION
+);
+
+$dir = dirname( __FILE__ ) . '/';
+$wgExtensionMessagesFiles['SlimboxThumbs'] = $dir . 'SlimboxThumbs.i18n.php';
+$wgHooks['BeforePageDisplay'][] = 'efSBTAddScripts';
+$wgAjaxExportList[] = 'efSBTGetImageSizes';
+
+// Ajax handler to get image sizes
+function efSBTGetImageSizes( $names ) {
+	$result = array();
+	foreach ( explode( ':', $names ) as $name ) {
+		if ( !isset( $result[$name] ) ) {
+			$title = Title::makeTitle( NS_FILE, $name );
+			if ( $title && $title->userCanRead() ) {
+				$file = wfFindFile( $title );
+				if ( $file && $file->getWidth() ) {
+					$result[ $name ] = array( $file->getWidth(), $file->getHeight(), $file->getFullUrl() );
+				}
+			}
+		}
+	}
+	return json_encode( $result );
+}
+
+// Adds javascript files and stylesheets.
+function efSBTAddScripts( $out ) {
+	global $wgVersion, $wgExtensionAssetsPath, $wgUploadPath, $wgServer, $wgScriptPath, $wgArticlePath;
+
+	$useExtensionPath = version_compare( $wgVersion, '1.16', '>=' ) &&
+		isset( $wgExtensionAssetsPath ) && $wgExtensionAssetsPath;
+	$eDir = ( $useExtensionPath ? $wgExtensionAssetsPath : $wgScriptPath . '/extensions' );
+	$eDir .= '/SlimboxThumbs/slimbox';
+
+	if ( substr( $wgVersion, 0, -2 ) < 1.16 ) {
+		$j = '$';
+		$out->addScript(
+			'<script type="text/javascript"'.
+			' src="http://ajax.googleapis.com/ajax/libs/jquery/1.3.2/jquery.min.js"></script>' . "\n"
+		);
+	} else {
+		$j = '$j';
+		$out->includeJQuery();
+	}
+
+	$re = str_replace( '\\$1', '[^:]+:(.*)', preg_quote( $wgArticlePath ) );
+
+	$out->addScript( '<script type="text/javascript" src="' . $eDir . '/js/slimbox2.js"></script>' . "\n" );
+	$out->addExtensionStyle( $eDir . '/css/slimbox2.css', 'screen' );
+	$out->addScript( '<script type="text/javascript" src="' . $eDir . '/slimboxthumbs.js"></script>' . "\n" );
+	$out->addInlineScript( "addHandler( window, 'load', function() {".
+		"makeSlimboxThumbs( $j, \"".addslashes( $re ).
+		"\", \"".addslashes( $wgServer.$wgScriptPath )."\" ); } );" );
+
+	return true;
+}
---
+++ slimbox/slimboxthumbs.js
@@ -0,0 +1,56 @@
+/**
+ * SlimboxThumbs extensions /rewritten/
+ * License: GNU GPL 3.0 or later
+ * Contributor(s): Vitaliy Filippov <vitalif@mail.ru>
+ */
+
+function makeSlimboxThumbs( $, pathRegexp, wgFullScriptPath ) {
+	var re = new RegExp( pathRegexp );
+	var canview = /\.(jpe?g|jpe|gif|png)$/i;
+	var m;
+	var names = [];
+	$( 'img' ).each( function( i, e ) {
+		if ( e.parentNode.nodeName == 'A' && ( m = re.exec( e.parentNode.href ) ) ) {
+			var n = decodeURIComponent( m[1] );
+			names.push( n );
+		}
+	} );
+	if ( names.length ) {
+		sajax_request_type = 'POST';
+		sajax_do_call( 'efSBTGetImageSizes', [ names.join( ':' ) ], function( r ) {
+			var nodes = [];
+			var can;
+			var ww = $( window ).width();
+			var wh = $( window ).height() * 0.9;
+			r = $.parseJSON( r.responseText );
+			$( 'img' ).each( function( i, e ) {
+				if ( e.parentNode.nodeName == 'A' && ( m = re.exec( e.parentNode.href ) ) ) {
+					var n = decodeURIComponent( m[1] );
+					if ( !r[n] ) {
+						return;
+					}
+					var h = r[n][2];
+					can = canview.exec( n );
+					if ( !can || r[n][0] > ww || r[n][1] > wh ) {
+						var sc = ww;
+						var sh = Math.round( r[n][0] * wh / r[n][1] );
+						if ( sh < sc ) {
+							sc = sh;
+						}
+						h = wgFullScriptPath + '/thumb.php?f=' + encodeURIComponent( n ) + '&w=' + sc;
+					}
+					if ( h != e.src ) {
+						e.parentNode._lightbox = [
+							h, '<a href="'+e.parentNode.href+'">'+
+							n.replace( /_/g, ' ' )+'</a>'
+						];
+						nodes.push( e.parentNode );
+					}
+				}
+			} );
+			$( nodes ).slimbox({ captionAnimationDuration: 0 }, function( e, i ) {
+				return e._lightbox;
+			}, function() { return true; });
+		} );
+	}
+}
